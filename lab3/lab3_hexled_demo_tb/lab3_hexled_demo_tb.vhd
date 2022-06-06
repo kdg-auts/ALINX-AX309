4e1f861dd47a933847fa40c879ffba5a4e090d5a
kdg-auts <karaman.dmitry@gmail.com>
2022-06-06 14:17:42 +0300

    testbench update

--- lab3/lab3_hexled_demo_tb/lab3_hexled_demo_tb.vhd
+++ lab3/lab3_hexled_demo_tb/lab3_hexled_demo_tb.vhd
@@ -17,12 +17,16 @@ architecture lab3_hexled_demo_tb_arch of lab3_hexled_demo_tb is
 	-- additional signals and parameters
 	constant clk_period : time := 20 ns;
 	constant virtual_clk_frq : natural := 4000; -- imitation of clk freq (is used to calculate all delay intervals)
+    constant BTN_PRESS : std_logic := '0'; -- value imitates button pressing (due to inverse logic)
+    constant BTN_RELEASE : std_logic := '1'; -- value imitates button release (due to inverse logic)
+    constant BTN_HOLD_PERIOD : time := clk_period*100;
+    constant BTN_BETW_PERIOD : time := clk_period*200;
 	
 	-- stimulus signals - signals mapped to the input and inout ports of tested entity
 	signal clk : std_logic;
 	signal rst : std_logic;
-	signal swi_test : std_logic_vector(3 downto 0) := (others => '0');
-	signal sw_prev, sw_next, sw_right, sw_left : std_logic := '0';
+	signal swi_test : std_logic_vector(3 downto 0) := (others => BTN_RELEASE);
+	signal sw_prev, sw_next, sw_right, sw_left : std_logic := BTN_RELEASE;
 
 	-- observed signals - signals mapped to the output ports of tested entity
 	signal segment_data : std_logic_vector(7 downto 0);
@@ -60,9 +64,9 @@ begin
 	-- reset signal generator
 	rst_gen_proc: process
 	begin
-		rst <= '1';
-		wait for clk_period*2;
 		rst <= '0';
+		wait for clk_period*2;
+		rst <= '1';
 		wait;
 	end process;
 
@@ -71,63 +75,72 @@ begin
 	-- test signal values generator
 	stim_gen_proc: process
 	begin
-		wait until rst = '0'; -- work only after reset
+		wait until rst = '1'; -- work only after reset
+        wait for BTN_BETW_PERIOD;
 		
 		-- position 0 value 0 - increment value once
-		sw_next <= '1';
-		wait for clk_period;
-		sw_next <= '0';
+		sw_next <= BTN_PRESS;
+		wait for BTN_HOLD_PERIOD;
+		sw_next <= BTN_RELEASE;
+        wait for BTN_BETW_PERIOD;
 
 		-- position 0 value 1 - decrement value twice (cause owerflow to value 31)
-		sw_prev <= '1';
-		wait for clk_period;
-		sw_prev <= '0';
-		wait for clk_period*10;
-		sw_prev <= '1';
-		wait for clk_period;
-		sw_prev <= '0';
+		sw_prev <= BTN_PRESS;
+		wait for BTN_HOLD_PERIOD;
+		sw_prev <= BTN_RELEASE;
+		wait for BTN_BETW_PERIOD;
+		sw_prev <= BTN_PRESS;
+		wait for BTN_HOLD_PERIOD;
+		sw_prev <= BTN_RELEASE;
+        wait for BTN_BETW_PERIOD;
 
 		-- position 0 value 31 - go to position 1
-		sw_left <= '1';
-		wait for clk_period;
-		sw_left <= '0';
+		sw_left <= BTN_PRESS;
+		wait for BTN_HOLD_PERIOD;
+		sw_left <= BTN_RELEASE;
+        wait for BTN_BETW_PERIOD;
 
 		-- position 1 value 0 - increment value once
-		sw_next <= '1';
-		wait for clk_period;
-		sw_next <= '0';
+		sw_next <= BTN_PRESS;
+		wait for BTN_HOLD_PERIOD;
+		sw_next <= BTN_RELEASE;
+        wait for BTN_BETW_PERIOD;
 
 		-- position 1 value 1 - decrement value twice (cause overflow to value 31)
-		sw_prev <= '1';
-		wait for clk_period;
-		sw_prev <= '0';
-		wait for clk_period*10;
-		sw_prev <= '1';
-		wait for clk_period;
-		sw_prev <= '0';
+		sw_prev <= BTN_PRESS;
+		wait for BTN_HOLD_PERIOD;
+		sw_prev <= BTN_RELEASE;
+		wait for BTN_BETW_PERIOD;
+		sw_prev <= BTN_PRESS;
+		wait for BTN_HOLD_PERIOD;
+		sw_prev <= BTN_RELEASE;
+        wait for BTN_BETW_PERIOD;
 
 		-- position 1 value 31 - go to position 5 backwards (cause overflow)
-		sw_right <= '1';
-		wait for clk_period;
-		sw_right <= '0';
-		wait for clk_period*10; -- position 0 value 31
-		sw_right <= '1';
-		wait for clk_period;
-		sw_right <= '0';
+		sw_right <= BTN_PRESS;
+		wait for BTN_HOLD_PERIOD;
+		sw_right <= BTN_RELEASE;
+		wait for BTN_BETW_PERIOD; -- position 0 value 31
+		sw_right <= BTN_PRESS;
+		wait for BTN_HOLD_PERIOD;
+		sw_right <= BTN_RELEASE;
+        wait for BTN_BETW_PERIOD;
 
 		-- position 5 value 0 - increment value once
-		sw_next <= '1';
-		wait for clk_period;
-		sw_next <= '0';
+		sw_next <= BTN_PRESS;
+		wait for BTN_HOLD_PERIOD;
+		sw_next <= BTN_RELEASE;
+        wait for BTN_BETW_PERIOD;
 
 		-- position 5 value 1 - decrement value twice (cause overflow to value 31)
-		sw_prev <= '1';
-		wait for clk_period;
-		sw_prev <= '0';
-		wait for clk_period*10;
-		sw_prev <= '1';
-		wait for clk_period;
-		sw_prev <= '0';
+		sw_prev <= BTN_PRESS;
+		wait for BTN_HOLD_PERIOD;
+		sw_prev <= BTN_RELEASE;
+		wait for BTN_BETW_PERIOD;
+		sw_prev <= BTN_PRESS;
+		wait for BTN_HOLD_PERIOD;
+		sw_prev <= BTN_RELEASE;
+        wait for BTN_BETW_PERIOD;
 
 		wait; -- stop forever
 	end process;
